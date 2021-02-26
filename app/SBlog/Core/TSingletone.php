b8e8d9ac0382863d2eeaf61f95b44fc40d3ae3d8
nigyl_pach <nigyl.pach@gmail.com>
2021-02-26 22:23:55 +0200

    Create confyg/init.php        Core/

--- app/SBlog/Core/TSingletone.php
+++ app/SBlog/Core/TSingletone.php
@@ -6,5 +6,11 @@ namespace App\SBlog\Core;
 
 trait TSingletone
 {
-
+    private static $instance;
+ public static  function  instance(){
+     if(self::$instance === null){
+         self::$instance = new self;
+     }
+     return self::$instance;
+ }
 }
